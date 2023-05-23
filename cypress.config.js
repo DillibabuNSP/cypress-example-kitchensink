dd174d5a14ab02de10860a1f63b2d369623e42ff
Dillibabu NSP <ak1661797@gmail.com>
2023-05-23 13:24:53 +0530

    inital commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,10 @@
-module.exports = {
-  'projectId': '4b7344',
-  e2e: {},
-}
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: 'cypress/e2e/**/*.js'
+  },
+});
